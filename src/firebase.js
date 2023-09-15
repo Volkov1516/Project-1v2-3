526c1f495a925b22a32984e7c7cb13c583532a62
Vlad Volkov <volkov.x@outlook.com>
2023-09-15 16:24:26 +0300

    Remove firebase config from .gitignore

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD7KdT95B3MThJ0A-ZyKOskkFixTPHsHIA",
+  authDomain: "journalisto-aae0f.firebaseapp.com",
+  projectId: "journalisto-aae0f",
+  storageBucket: "journalisto-aae0f.appspot.com",
+  messagingSenderId: "255587758716",
+  appId: "1:255587758716:web:f98ed7b7bfece484820ff9"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export default app;
