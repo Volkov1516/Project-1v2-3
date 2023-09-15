03515e43b8acb3bc62f60ba39ff1e48b22957bd6
Vlad Volkov <volkov.x@outlook.com>
2023-09-15 16:46:01 +0300

    Add login with Google

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,6 +1,11 @@
 import { useState } from 'react';
 import { auth } from 'firebase.js';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider
+} from 'firebase/auth';
 
 import css from './Auth.module.css';
 import logo from 'assets/logo-black.png';
@@ -34,9 +39,31 @@ export const Auth = () => {
         .catch((error) => {
           const errorCode = error.code;
           const errorMessage = error.message;
-          console.log(errorCode, errorMessage)
+          console.log(errorCode, errorMessage);
         });
     }
+  };
+
+  const handleGoogle = async () => {
+    const provider = new GoogleAuthProvider();
+
+    await signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        console.log(token);
+
+        const user = result.user;
+        console.log(user);
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        console.log(email, credential);
+      });
   };
 
   return (
@@ -80,10 +107,7 @@ export const Auth = () => {
           </div>
           <button type="submit" onClick={onSubmit}>Continue with email</button>
         </form>
-        <button>Continue with Google</button>
-        <button>Continue with Apple</button>
-        <button>Continue with Microsoft</button>
-        <button>Continue with Facebook</button>
+        <button onClick={handleGoogle}>Continue with Google</button>
         {authType === 'Sing in'
           ? <>
             <span>Don't have an account?</span>
