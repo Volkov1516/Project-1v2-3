98e4c5b2f6afde38d3cd8b89d64c00e207787ff5
Vlad Volkov <volkov.x@outlook.com>
2023-09-16 14:01:15 +0300

    Set dlobal font
    - Mock article for font tests

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -15,6 +15,11 @@ export const Home = () => {
   return (
     <div>
       <button onClick={handleSignOut}>Sign out</button>
+      <h1 style={{fontWeight: 800, lineHeight: "1.2em", width: "fit-content", paddingRight: "16px"}}>MISS YOU</h1>
+      <h1 style={{fontWeight: 800, lineHeight: "1.2em", color: "white", backgroundColor: "#1971c2", width: "fit-content", paddingRight: "16px"}}>DIAMONDS AND RUST</h1>
+      <p style={{width: "600px", fontWeight: 400, lineHeight: "1.5em"}}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+      </p>
     </div>
   );
 };
