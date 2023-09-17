9887b1b79f74fe7ee4e3813169e70184cece37f7
Vlad Volkov <volkov.x@outlook.com>
2023-09-17 13:22:50 +0300

    Create Loading component

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,23 @@ import { onAuthStateChanged } from 'firebase/auth';
 
 import { Auth } from 'components/Auth/Auth';
 import { Home } from 'components/Home/Home';
+import { Loading } from 'components/Loading/Loading';
 
 export const App = () => {
+  const [loading, setLoading] = useState(true);
   const [isAuth, setIsAuth] = useState(false);
 
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
         setIsAuth(true);
+        setLoading(false);
       } else {
         setIsAuth(false);
+        setLoading(false);
       }
     });
   }, []);
 
-  return (
-    <>
-      {isAuth ? <Home /> : <Auth />}
-    </>
-  );
+  return loading ? <Loading /> : isAuth ? <Home /> : <Auth />;
 };
