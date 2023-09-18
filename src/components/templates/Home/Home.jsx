0fe6a8ea7ca36e840d512e23f963e5bfa6e7360c
Vlad Volkov <volkov.x@outlook.com>
2023-09-18 11:01:15 +0300

    Refactor Home

--- src/components/templates/Home/Home.jsx
+++ src/components/templates/Home/Home.jsx
@@ -1,41 +1,29 @@
-import { db } from 'firebase.js';
-import { collection, addDoc, getDocs } from 'firebase/firestore';
-
 import css from './Home.module.css';
 
 import { Header } from 'components/molecules/Header/Header';
 
 export const Home = () => {
-  const handlePost = async () => {
-    try {
-      const docRef = await addDoc(collection(db, "users"), {
-        first: "Ada",
-        last: "Lovelace",
-        born: 1815
-      });
-      console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-  };
-
-  const handleGet = async () => {
-    const querySnapshot = await getDocs(collection(db, "users"));
-    querySnapshot.forEach((doc) => {
-      console.log(`${doc.id} => ${doc.data()}`);
-    });
-  };
-
   return (
     <div className={css.container}>
       <Header />
-      <button onClick={handlePost}>POST</button>
-      <button onClick={handleGet}>GET</button>
-      <h1 style={{ fontWeight: 800, lineHeight: "1.2em", width: "fit-content", paddingRight: "16px" }}>MISS YOU</h1>
-      <h1 style={{ fontWeight: 800, lineHeight: "1.2em", color: "white", backgroundColor: "#1971c2", width: "fit-content", paddingRight: "16px" }}>DIAMONDS AND RUST</h1>
-      <p style={{ width: "600px", fontWeight: 400, lineHeight: "1.5em" }}>
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-      </p>
+      <div className={css.main}>
+        <h1 style={{ cursor: "pointer", fontSize: "40px", fontWeight: 800, lineHeight: "1.2em", width: "fit-content", paddingRight: "16px" }}>MISS YOU</h1>
+        <h1 style={{ cursor: "pointer", fontSize: "40px", fontWeight: 800, lineHeight: "1.2em", color: "white", backgroundColor: "#1971c2", width: "fit-content", paddingRight: "16px" }}>DIAMONDS AND RUST</h1>
+        <h1 style={{ cursor: "pointer", fontSize: "40px", fontWeight: 800, lineHeight: "1.2em", width: "fit-content", paddingRight: "16px" }}>ANGIE</h1>
+        <h1 style={{ cursor: "pointer", fontSize: "40px", fontWeight: 800, lineHeight: "1.2em", width: "fit-content", paddingRight: "16px" }}>ROSENROT</h1>
+        <h1 style={{ cursor: "pointer", fontSize: "40px", fontWeight: 800, lineHeight: "1.2em", width: "fit-content", paddingRight: "16px" }}>SOMENTING ON THE WAY</h1>
+        <h1 style={{ cursor: "pointer", fontSize: "40px", fontWeight: 800, lineHeight: "1.2em", width: "fit-content", paddingRight: "16px" }}>BREAKING THE LAW</h1>
+        <h1 style={{ cursor: "pointer", fontSize: "40px", fontWeight: 800, lineHeight: "1.2em", width: "fit-content", paddingRight: "16px" }}>SONNE</h1>
+        <h1 style={{ cursor: "pointer", fontSize: "40px", fontWeight: 800, lineHeight: "1.2em", width: "fit-content", paddingRight: "16px" }}>BRINGIN ON THE HEARTBREAK</h1>
+        <h1 style={{ cursor: "pointer", fontSize: "40px", fontWeight: 800, lineHeight: "1.2em", width: "fit-content", paddingRight: "16px" }}>LOVE BITES</h1>
+        <h1 style={{ cursor: "pointer", fontSize: "40px", fontWeight: 800, lineHeight: "1.2em", color: "white", backgroundColor: "#e03131", width: "fit-content", paddingRight: "16px" }}>STAIRWAY TO HEAVEN</h1>
+        <h1 style={{ cursor: "pointer", fontSize: "40px", fontWeight: 800, lineHeight: "1.2em", width: "fit-content", paddingRight: "16px" }}>HOLD ON THE LINE</h1>
+        <h1 style={{ cursor: "pointer", fontSize: "40px", fontWeight: 800, lineHeight: "1.2em", width: "fit-content", paddingRight: "16px" }}>HYSTERIA</h1>
+        <h1 style={{ cursor: "pointer", fontSize: "40px", fontWeight: 800, lineHeight: "1.2em", width: "fit-content", paddingRight: "16px" }}>SAD BUT TRUE</h1>
+        <h1 style={{ cursor: "pointer", fontSize: "40px", fontWeight: 800, lineHeight: "1.2em", width: "fit-content", paddingRight: "16px" }}>HAIFISCH</h1>
+        <h1 style={{ cursor: "pointer", fontSize: "40px", fontWeight: 800, lineHeight: "1.2em", width: "fit-content", paddingRight: "16px" }}>LONELY NIGHTS</h1>
+        <h1 style={{ cursor: "pointer", fontSize: "40px", fontWeight: 800, lineHeight: "1.2em", width: "fit-content", paddingRight: "16px" }}>SLEEPING</h1>
+      </div>
     </div>
   );
 };
