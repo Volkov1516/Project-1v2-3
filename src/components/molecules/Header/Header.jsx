b383aa2d40c3c3075f68c8ff344ca5dc65cd427d
Vlad Volkov <volkov.x@outlook.com>
2023-09-18 18:03:25 +0300

    Add custom dropdown element

--- src/components/molecules/Header/Header.jsx
+++ src/components/molecules/Header/Header.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { auth } from 'firebase.js';
 import { signOut } from 'firebase/auth';
 
@@ -6,6 +8,8 @@ import css from './Header.module.css';
 import { Button } from 'components/atoms/Button/Button';
 
 export const Header = () => {
+  const [open, setOpen] = useState(false);
+
   const handleSignOut = () => {
     signOut(auth).then(() => {
       console.log('Signed out successfully');
@@ -20,13 +24,24 @@ export const Header = () => {
     <div className={css.container}>
       <div className={css.left}>
         <Button variant="contained" size="large">CREATE</Button>
-        <Button variant="text">categories</Button>
+        <Button variant="text" onClick={() => setOpen(!open)}>all articles</Button>
         <Button variant="text">search</Button>
       </div>
       <div className={css.right}>
-      <Button variant="text">settings</Button>
-      <Button variant="text" onClick={handleSignOut}>signout</Button>
+        <Button variant="text">settings</Button>
+        <Button variant="text" onClick={handleSignOut}>signout</Button>
       </div>
+      {open && (
+        <div className={css.dropdown}>
+          <div className={css.dropdownItem}>add category</div>
+          <div className={css.dropdownItem}>all articles</div>
+          <div className={css.dropdownItem} style={{color: "#1971c2"}}>#goals</div>
+          <div className={css.dropdownItem} style={{color: "#1971c2"}}>#projects</div>
+          <div className={css.dropdownItem} style={{ width: "fit-content", backgroundColor: "#e03131", paddingRight: "12px" }}>red</div>
+          <div className={css.dropdownItem} style={{ width: "fit-content", color: "white", backgroundColor: "#1971c2", paddingRight: "12px" }}>blue</div>
+          <div className={css.dropdownItem}>trash</div>
+        </div>
+      )}
     </div>
   );
 };
