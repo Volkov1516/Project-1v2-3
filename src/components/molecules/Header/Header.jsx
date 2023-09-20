9cd7a2b00d891a84cdbcadf57bf2d98cc3ab4476
Vlad Volkov <volkov.x@outlook.com>
2023-09-20 11:30:03 +0300

    Ad user dropdown

--- src/components/molecules/Header/Header.jsx
+++ src/components/molecules/Header/Header.jsx
@@ -5,11 +5,12 @@ import { signOut } from 'firebase/auth';
 
 import css from './Header.module.css';
 
-import Button  from 'components/atoms/Button/Button';
+import Button from 'components/atoms/Button/Button';
 import { ModalEditor } from 'components/molecules/ModalEditor/ModalEditor';
 
 export const Header = () => {
-  const [open, setOpen] = useState(false);
+  const [categoriesMenu, setCategoriesMenu] = useState(false);
+  const [userMenu, setUserMenu] = useState(false);
 
   const handleSignOut = () => {
     signOut(auth).then(() => {
@@ -25,15 +26,14 @@ export const Header = () => {
     <div className={css.container}>
       <div className={css.left}>
         <ModalEditor />
-        <Button variant="text" onClick={() => setOpen(!open)} onMouseOver={() => setOpen(true)} onMouseLeave={() => setOpen(false)}>all articles</Button>
-        <Button variant="text">search</Button>
+        <Button variant="text" onClick={() => setCategoriesMenu(!categoriesMenu)} onMouseOver={() => setCategoriesMenu(true)} onMouseLeave={() => setCategoriesMenu(false)}>all articles</Button>
+        <div className={css.serachBtn}><Button variant="text">search</Button></div>
       </div>
       <div className={css.right}>
-        <Button variant="text">settings</Button>
-        <Button variant="text" onClick={handleSignOut}>signout</Button>
+        <Button variant="text" onClick={() => setUserMenu(!userMenu)} onMouseOver={() => setUserMenu(true)} onMouseLeave={() => setUserMenu(false)}>user</Button>
       </div>
-      {open && (
-        <div className={css.dropdown} onMouseOver={() => setOpen(true)} onMouseLeave={() => setOpen(false)}>
+      {categoriesMenu && (
+        <div className={css.dropdown} onMouseOver={() => setCategoriesMenu(true)} onMouseLeave={() => setCategoriesMenu(false)}>
           <div className={css.dropdownItem}>add category</div>
           <div className={css.dropdownItem}>all articles</div>
           <div className={css.dropdownItem} style={{ color: "#1971c2" }}>#goals</div>
@@ -41,6 +41,15 @@ export const Header = () => {
           <div className={css.dropdownItem} style={{ width: "fit-content", color: "white", backgroundColor: "#e03131", paddingRight: "12px" }}>red</div>
           <div className={css.dropdownItem} style={{ width: "fit-content", color: "white", backgroundColor: "#1971c2", paddingRight: "12px" }}>blue</div>
           <div className={css.dropdownItem}>trash</div>
+        </div>
+      )}
+      {userMenu && (
+        <div className={`${css.userDropdown} ${css.userDropdown}`} onMouseOver={() => setUserMenu(true)} onMouseLeave={() => setUserMenu(false)}>
+          <div className={css.dropdownItem}>settings</div>
+          <div className={css.dropdownItem}>dark theme</div>
+          <div className={css.dropdownItem}>eye saver</div>
+          <div className={`${css.dropdownItem} ${css.dropdownViewItem}`}>column view</div>
+          <div className={css.dropdownItem} style={{ color: "#1971c2" }} onClick={handleSignOut}>sign out</div>
         </div>
       )}
     </div>
