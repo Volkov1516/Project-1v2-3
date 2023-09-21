1195b2d6fdbd19ed6999f5a30279097c6cfd1292
Vlad Volkov <volkov.x@outlook.com>
2023-09-21 11:49:50 +0300

    Add tools for text toolbar

--- src/components/organisms/Editor/plugins/FloatingTextToolbarPlugin/FloatingTextToolbarPlugin.jsx
+++ src/components/organisms/Editor/plugins/FloatingTextToolbarPlugin/FloatingTextToolbarPlugin.jsx
@@ -1,10 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
 import {
+  FORMAT_TEXT_COMMAND,
   COMMAND_PRIORITY_LOW,
   SELECTION_CHANGE_COMMAND,
   $getSelection,
+  $isRangeSelection,
+  $createParagraphNode,
+  $isTextNode,
+  $isLinkNode
 } from 'lexical';
+import {
+  $patchStyleText,
+  $setBlocksType
+} from '@lexical/selection';
 
 import css from './FloatingTextToolbarPlugin.module.css';
 
@@ -20,10 +29,6 @@ export const FloatingTextToolbarPlugin = () => {
         const selection = $getSelection();
         const nativeSelection = window.getSelection();
         const rootElement = editor.getRootElement();
-
-        console.log(selection);
-        console.log(nativeSelection);
-        console.log(rootElement);
 
         if (
           selection !== null &&
@@ -42,5 +47,47 @@ export const FloatingTextToolbarPlugin = () => {
     )
   }, [editor]);
 
-  return isText && <div className={css.container}>FloatingTextToolbarPlugin</div>;
+  const clearFormat = () => {
+    editor.update(() => {
+      const selection = $getSelection();
+      if ($isRangeSelection(selection)) {
+        selection.getNodes().forEach((node) => {
+          if ($isTextNode(node)) {
+            node.setFormat(0);
+            node.setStyle("");
+          } else if ($isLinkNode(node)) {
+            const children = node.getChildren();
+            for (const child of children) {
+              node.insertBefore(child);
+            }
+            node.remove();
+          }
+        });
+        $setBlocksType(selection, () => $createParagraphNode());
+      }
+    });
+  };
+
+  const formatTextColor = () => {
+    editor.update(() => {
+      const selection = $getSelection();
+
+      if ($isRangeSelection(selection)) {
+        $patchStyleText(selection, { color: '#F55050' });
+      }
+    });
+  };
+
+  return (isText && <div className={css.container}>
+    <button onClick={() => clearFormat()}>clear</button>
+    <button onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'bold')}>bold</button>
+    <button onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'italic')}>italic</button>
+    <button onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'underline')}>underline</button>
+    <button onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'strikethrough')}>strikethrough</button>
+    <button onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'subscript')}>subscript</button>
+    <button onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'superscript')}>superscript</button>
+    <button onClick={() => formatTextColor()}>color</button>
+    <button onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'highlight')}>highlight</button>
+    <button onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'code')}>code</button>
+  </div>);
 };
