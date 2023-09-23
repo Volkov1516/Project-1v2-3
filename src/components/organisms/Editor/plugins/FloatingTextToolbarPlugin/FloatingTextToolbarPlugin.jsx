2a42ec10adfd5eaec1e930570f53e2bf0f58da72
Vlad Volkov <volkov.x@outlook.com>
2023-09-23 16:00:02 +0300

    Add cope/cut/paste operations

--- src/components/organisms/Editor/plugins/FloatingTextToolbarPlugin/FloatingTextToolbarPlugin.jsx
+++ src/components/organisms/Editor/plugins/FloatingTextToolbarPlugin/FloatingTextToolbarPlugin.jsx
@@ -8,6 +8,9 @@ import {
   $getSelection,
   $isRangeSelection,
   BLUR_COMMAND,
+  COPY_COMMAND,
+  CUT_COMMAND,
+  PASTE_COMMAND
 } from 'lexical';
 import {
   $patchStyleText
@@ -79,8 +82,40 @@ export const FloatingTextToolbarPlugin = ({ modalEditorContentRef }) => {
     });
   };
 
+  const paste =  () => {
+    navigator.clipboard.read().then(async (...args) => {
+      const data = new DataTransfer();
+
+      const items = await navigator.clipboard.read();
+      const item = items[0];
+
+      const permission = await navigator.permissions.query({
+        // @ts-ignore These types are incorrect.
+        name: 'clipboard-read',
+      });
+      if (permission.state === 'denied') {
+        alert('Not allowed to paste from clipboard.');
+        return;
+      }
+
+      for (const type of item.types) {
+        const dataString = await (await item.getType(type)).text();
+        data.setData(type, dataString);
+      }
+
+      const event = new ClipboardEvent('paste', {
+        clipboardData: data,
+      });
+
+      editor.dispatchCommand(PASTE_COMMAND, event);
+    });
+  };
+
   return (modalEditorContentRef?.current && isText && createPortal(
     <div className={css.container} style={{ top: top }}>
+      <button id="tool" onClick={() => editor.dispatchCommand(COPY_COMMAND, null)}>Copy</button>
+      <button id="tool" onClick={() => editor.dispatchCommand(CUT_COMMAND, null)}>Cut</button>
+      <button id="tool" onClick={paste}>Paste</button>
       <button id="tool" onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'bold')}>B</button>
       <button id="tool" onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'italic')}>I</button>
       <button id="tool" onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'underline')}>U</button>
