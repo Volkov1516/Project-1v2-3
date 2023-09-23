97009d1b083872fa62d01e28360d79080eeecb85
Vlad Volkov <volkov.x@outlook.com>
2023-09-23 13:22:25 +0300

    Display toolbar in the middle

--- src/components/organisms/Editor/plugins/FloatingTextToolbarPlugin/FloatingTextToolbarPlugin.jsx
+++ src/components/organisms/Editor/plugins/FloatingTextToolbarPlugin/FloatingTextToolbarPlugin.jsx
@@ -20,7 +20,6 @@ export const FloatingTextToolbarPlugin = ({ modalEditorContentRef }) => {
 
   const [isText, setIsText] = useState(false);
   const [top, setTop] = useState(0);
-  const [left, setLeft] = useState(0);
 
   useEffect(() => {
     editor.registerCommand(
@@ -39,26 +38,13 @@ export const FloatingTextToolbarPlugin = ({ modalEditorContentRef }) => {
         ) {
           const viewportWidth = window.visualViewport.width;
 
-          if (viewportWidth < 640) {
-            const domRange = nativeSelection.getRangeAt(0);
-            const rect = domRange.getBoundingClientRect();
-            const textRect = modalEditorContentRef?.current?.getBoundingClientRect();
+          const domRange = nativeSelection.getRangeAt(0);
+          const rect = domRange.getBoundingClientRect();
+          const textRect = modalEditorContentRef?.current?.getBoundingClientRect();
 
-            const top = rect?.top - textRect?.top + modalEditorContentRef?.current?.scrollTop - 30;
+          const top = rect?.top - textRect?.top + modalEditorContentRef?.current?.scrollTop - 48;
 
-            setTop(top);
-            setLeft(0);
-          }
-          else {
-            const domRange = nativeSelection.getRangeAt(0);
-            const rect = domRange.getBoundingClientRect();
-            const textRect = modalEditorContentRef?.current?.getBoundingClientRect();
-
-            const top = rect?.top - textRect?.top + modalEditorContentRef?.current?.scrollTop - 30;
-
-            setTop(top);
-            setLeft(rect.left);
-          }
+          setTop(top);
 
           setIsText(true);
         }
@@ -96,13 +82,13 @@ export const FloatingTextToolbarPlugin = ({ modalEditorContentRef }) => {
   };
 
   return (modalEditorContentRef?.current && isText && createPortal(
-    <div className={css.container} style={{ top: top, left: left }}>
+    <div className={css.container} style={{ top: top }}>
       <button id="tool" onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'bold')}>B</button>
       <button id="tool" onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'italic')}>I</button>
       <button id="tool" onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'underline')}>U</button>
       <button id="tool" onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'strikethrough')}>S</button>
-      <button id="tool" onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'subscript')}>Sub</button>
-      <button id="tool" onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'superscript')}>Sup</button>
+      <button id="tool" onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'subscript')}>S</button>
+      <button id="tool" onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'superscript')}>S</button>
       <button id="tool" onClick={() => formatTextColor()}>C</button>
       <button id="tool" onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'highlight')}>H</button>
       <button id="tool" onClick={() => editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'code')}>C</button>
