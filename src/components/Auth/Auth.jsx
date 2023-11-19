86d3707a64aedacc8dfa5c54a39e5af7b3c2f8ea
Vlad Volkov <volkov.x@outlook.com>
2023-11-19 20:46:12 +0200

    Reset password

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -4,7 +4,8 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signInWithPopup,
-  GoogleAuthProvider
+  GoogleAuthProvider,
+  sendPasswordResetEmail
 } from 'firebase/auth';
 import css from './Auth.module.css';
 import logo from 'assets/logo.png';
@@ -16,6 +17,9 @@ export default function Auth() {
   const [password, setPassword] = useState('');
   const [focussedEmail, setFocussedEmail] = useState(false);
   const [focussedPassword, setFocussedPassword] = useState(false);
+  const [reset, setReset] = useState(false);
+  const [resetEmail, setResetEmail] = useState('');
+  const [resetMessage, setResetMessage] = useState(false);
 
   const handleFocus = (e) => {
     switch (e.target.name) {
@@ -74,6 +78,20 @@ export default function Auth() {
       });
   };
 
+  const handleResetPassword = async () => {
+    if (!resetEmail) return;
+
+    await sendPasswordResetEmail(auth, resetEmail)
+      .then(() => setResetMessage(true))
+      .catch(error => console.log(error));
+  };
+
+  const handleCloseReset = () => {
+    setResetEmail('');
+    setResetMessage(false);
+    setReset(false);
+  };
+
   return (
     <div className={css.container}>
       <div className={css.header}>
@@ -82,58 +100,75 @@ export default function Auth() {
           <span className={css.logoText}>Journalisto</span>
         </div>
       </div>
-      <div className={css.content}>
-        <div className={css.title}>{authType}</div>
-        <form className={css.form} onSubmit={onSubmit}>
-          <div className={css.fieldContainer}>
-            <label className={css.label} htmlFor="email">Email</label>
-            <input
-              className={css.input}
-              id="email"
-              name="email"
-              type="email"
-              placeholder="Enter your email"
-              required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              onBlur={handleFocus}
-              data-focussed={focussedEmail.toString()}
-              error="Invalid email address"
-            />
-            <span className={css.error}>Invalid email address</span>
+      {!reset ?
+        (
+          <div className={css.content}>
+            <div className={css.title}>{authType}</div>
+            <form className={css.form} onSubmit={onSubmit}>
+              <div className={css.fieldContainer}>
+                <label className={css.label} htmlFor="email">Email</label>
+                <input
+                  className={css.input}
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="Enter your email"
+                  required
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  onBlur={handleFocus}
+                  data-focussed={focussedEmail.toString()}
+                  error="Invalid email address"
+                />
+                <span className={css.error}>Invalid email address</span>
+              </div>
+              <div className={css.fieldContainer}>
+                <label className={css.label} htmlFor="password">Password</label>
+                <input
+                  className={css.input}
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="Enter your password"
+                  pattern="[0-9a-zA-Z]{6,}"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  onBlur={handleFocus}
+                  data-focussed={focussedPassword.toString()}
+                />
+                <span className={css.error}>Password must contain minimum 6 characters</span>
+              </div>
+              <button className={css.submitBtn} type="submit">Continue with email</button>
+              {authType === "Log in" && <button className={css.forgotPasswordButton} onClick={() => setReset(true)}>Forgot password</button>}
+              <hr className={css.divider} />
+              <button className={css.googleBtn} onClick={handleGoogle}><img className={css.buttonImg} src={google} alt="google" />Continue with Google</button>
+            </form>
+            {authType === "Log in"
+              ? <>
+                <span className={css.alternativeText}>Don't have an account?</span>
+                <button className={css.toggleBtn} onClick={() => handleToggleAuth("Sing up")}>Create account</button>
+              </>
+              : <>
+                <span className={css.alternativeText}>Already have an account?</span>
+                <button className={css.toggleBtn} onClick={() => handleToggleAuth("Log in")}>Log in</button>
+              </>
+            }
           </div>
-          <div className={css.fieldContainer}>
-            <label className={css.label} htmlFor="password">Password</label>
-            <input
-              className={css.input}
-              id="password"
-              name="password"
-              type="password"
-              placeholder="Enter your password"
-              pattern="[0-9a-zA-Z]{6,}"
-              required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              onBlur={handleFocus}
-              data-focussed={focussedPassword.toString()}
-            />
-            <span className={css.error}>Password must contain minimum 6 characters</span>
+        )
+        :
+        (
+          <div className={css.forgotPasswordContainer}>
+            <div className={css.title}>Reset Password</div>
+            <div className={css.resetForm}>
+              <input className={css.input} type="email" placeholder="Enter your email" value={resetEmail} onChange={(e) => setResetEmail(e.target.value)} />
+              <button className={css.resetButton} onClick={handleResetPassword}>Send email</button>
+            </div>
+            {resetMessage && <p className={css.resetMessage}>Check your email to proceed password reset.</p>}
+            <button className={css.resetBackButton} onClick={handleCloseReset}>Back to Login</button>
           </div>
-          <button className={css.submitBtn} type="submit">Continue with email</button>
-          <hr className={css.divider} />
-          <button className={css.googleBtn} onClick={handleGoogle}><img className={css.buttonImg} src={google} alt="google" />Continue with Google</button>
-        </form>
-        {authType === "Log in"
-          ? <>
-            <span className={css.alternativeText}>Don't have an account?</span>
-            <button className={css.toggleBtn} onClick={() => handleToggleAuth("Sing up")}>Create account</button>
-          </>
-          : <>
-            <span className={css.alternativeText}>Already have an account?</span>
-            <button className={css.toggleBtn} onClick={() => handleToggleAuth("Log in")}>Log in</button>
-          </>
-        }
-      </div>
+        )
+      }
     </div>
   );
 };
