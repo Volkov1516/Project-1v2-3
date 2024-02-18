591c5957ace82d2912020d87eb33fadbe40f1bc5
Vlad Volkov <volkov.x@outlook.com>
2024-02-18 14:08:43 +0200

    styles

--- src/components/atoms/IconButton/IconButton.jsx
+++ src/components/atoms/IconButton/IconButton.jsx
@@ -2,14 +2,13 @@ import css from './IconButton.module.css';
 
 export const IconButton = ({
   primary,
-  size = 'small',
   path,
   viewBox = '0 -960 960 960',
   onClick
 }) => {
   return (
-    <button className={`${primary && css.primary}`} onClick={onClick}>
-      <svg className={`${css[size]}`} viewBox={viewBox} xmlns="http://www.w3.org/2000/svg">
+    <button id="iconButton" className={`${css.button} ${primary && css.primary}`} onClick={onClick}>
+      <svg className={css.svg} viewBox={viewBox} xmlns="http://www.w3.org/2000/svg">
         <path d={path} />
       </svg>
     </button>
