8ebcb376607ce80ff3490ddbbdb3d7aab3dca18f
Vlad Volkov <volkov.x@outlook.com>
2024-02-20 11:12:04 +0200

    button effects

--- src/components/atoms/IconButton/IconButton.jsx
+++ src/components/atoms/IconButton/IconButton.jsx
@@ -6,8 +6,19 @@ export const IconButton = ({
   viewBox = '0 -960 960 960',
   onClick
 }) => {
+  const handleClick = (e) => {
+    const element = e.currentTarget;
+
+    element.style.active = null;
+    element.classList.add(css.pressed);
+
+    setTimeout(function() {
+      element.classList.remove(css.pressed);
+    }, 100);
+  };
+
   return (
-    <button id="iconButton" className={`${css.button} ${primary && css.primary}`} onClick={onClick}>
+    <button id="iconButton" className={`${css.button} ${primary && css.primary}`} onClick={onClick} onTouchStart={handleClick}>
       <svg className={css.svg} viewBox={viewBox} xmlns="http://www.w3.org/2000/svg">
         <path d={path} />
       </svg>
