32587b5e9df7890200518e44deed5f7c378f889d
Vlad Volkov <volkov.x@outlook.com>
2024-03-21 18:35:08 +0200

    task: aontentEditable

--- src/components/pages/Home/Manager/Tasks/Task/Task.jsx
+++ src/components/pages/Home/Manager/Tasks/Task/Task.jsx
@@ -1,8 +1,7 @@
-// import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { setSnackbar } from 'redux/features/app/appSlice';
-// import { setActiveTaskId, updateDocuments } from 'redux/features/user/userSlice';
-import { updateDocuments } from 'redux/features/user/userSlice';
+import { setActiveTaskId, updateDocuments } from 'redux/features/user/userSlice';
 import { db } from 'firebase.js';
 import { doc, setDoc } from 'firebase/firestore';
 
@@ -15,19 +14,17 @@ import { findFolder } from 'utils/findFolder';
 export const Task = ({ id, content, isDraggable }) => {
   const dispatch = useDispatch();
 
-  // const textareaRef = useRef(null);
+  const { userId, documents, path, activeTaskId } = useSelector(state => state.user);
 
-  // const { userId, documents, path, activeTaskId } = useSelector(state => state.user);
-  const { userId, documents, path } = useSelector(state => state.user);
+  const contentEditableRef = useRef(null);
 
-  // const [value, setValue] = useState(content);
+  const [initialContent, setInitialContent] = useState('');
 
-  // useEffect(() => {
-  //   if(!isDraggable) {
-  //     textareaRef.current.style.height = 'auto';
-  //     textareaRef.current.style.height = textareaRef.current.scrollHeight + 'px';
-  //   }
-  // }, [id, isDraggable]);
+  useEffect(() => {
+    if (contentEditableRef.current && id === activeTaskId) {
+      contentEditableRef.current.focus();
+    }
+  }, [id, activeTaskId]);
 
   const handleDeleteTask = async () => {
     try {
@@ -56,63 +53,67 @@ export const Task = ({ id, content, isDraggable }) => {
     }
   };
 
-  // const handleOnChange = (e) => {
-  //   setValue(e.target.value);
+  const handleOnClick = e => {
+    setInitialContent(e.target.innerText);
+  };
 
-  //   e.target.style.height = 'auto';
-  //   e.target.style.height = e.target.scrollHeight + 'px';
-  // };
+  const handleOnBlur = async e => {
+    if (!e.target.innerText) {
+      handleDeleteTask();
+    }
+    else if (initialContent === e.target.innerText) {
+      return;
+    }
+    else {
+      try {
+        const newDocuments = JSON.parse(JSON.stringify(documents));
 
-  // const handleOnBlur = async (e) => {
-  //   if (!value) {
-  //     handleDeleteTask();
-  //   }
-  //   else {
-  //     try {
-  //       const newDocuments = JSON.parse(JSON.stringify(documents));
+        const createTask = (targetFolder) => {
+          if (targetFolder.tasks && targetFolder.tasks.length > 0) {
+            for (let i = 0; i < targetFolder.tasks.length; i++) {
+              if (targetFolder.tasks[i].id === id) {
+                targetFolder.tasks[i].content = e.target.innerText;
+              }
+            }
+          }
+        };
 
-  //       const createTask = (targetFolder) => {
-  //         if (targetFolder.tasks && targetFolder.tasks.length > 0) {
-  //           for (let i = 0; i < targetFolder.tasks.length; i++) {
-  //             if (targetFolder.tasks[i].id === id) {
-  //               targetFolder.tasks[i].content = e.target.value;
-  //             }
-  //           }
-  //         }
-  //       };
+        findFolder(newDocuments, path[path.length - 1], createTask);
 
-  //       findFolder(newDocuments, path[path.length - 1], createTask);
+        dispatch(updateDocuments(newDocuments));
 
-  //       dispatch(updateDocuments(newDocuments));
+        await setDoc(doc(db, 'users', userId), { documents: newDocuments }, { merge: true });
 
-  //       await setDoc(doc(db, 'users', userId), { documents: newDocuments }, { merge: true });
+        dispatch(setSnackbar('Task was saved'));
+      } catch (error) {
+        dispatch(setSnackbar('Failed to save the task'));
+      }
+    }
 
-  //       dispatch(setSnackbar('Task was saved'));
-  //     } catch (error) {
-  //       dispatch(setSnackbar('Failed to save the task'));
-  //     }
-  //   }
+    setInitialContent('');
+    dispatch(setActiveTaskId(null));
+  };
 
-  //   dispatch(setActiveTaskId(null));
-  // };
+  const handleOnKeyDown = e => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+
+      e.target.blur();
+    }
+  };
 
   return (
     <div className={css.container}>
       <IconButton onClick={handleDeleteTask} path="M480.134-120q-74.673 0-140.41-28.339-65.737-28.34-114.365-76.922-48.627-48.582-76.993-114.257Q120-405.194 120-479.866q0-74.673 28.339-140.41 28.34-65.737 76.922-114.365 48.582-48.627 114.257-76.993Q405.194-840 479.866-840q74.673 0 140.41 28.339 65.737 28.34 114.365 76.922 48.627 48.582 76.993 114.257Q840-554.806 840-480.134q0 74.673-28.339 140.41-28.34 65.737-76.922 114.365-48.582 48.627-114.257 76.993Q554.806-120 480.134-120ZM480-160q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
-      {/* {isDraggable
-        ? (<p>{value}</p>)
-        : (<textarea
-          ref={textareaRef}
-          className={css.textarea}
-          name="task"
-          autoFocus={id === activeTaskId}
-          rows={1}
-          value={value}
-          onChange={handleOnChange}
-          onBlur={handleOnBlur}
-        />)
-      } */}
-      <div className={css.content} contentEditable={!isDraggable}>
+      <div
+        ref={contentEditableRef}
+        className={css.content}
+        contentEditable={!isDraggable}
+        suppressContentEditableWarning={true}
+        onClick={handleOnClick}
+        onBlur={handleOnBlur}
+        onKeyDown={handleOnKeyDown}
+      >
         {content}
       </div>
     </div>
