f6828ea00bf0e4796b98bb614c8b0285e617c4a9
Vlad Volkov <volkov.x@outlook.com>
2024-03-24 11:24:59 +0200

    delete document and modal slices

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,15 +3,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import appReducer from './features/app/appSlice';
 import userReducer from './features/user/userSlice';
 import noteReducer from './features/note/noteSlice';
-import documentReducer from './features/document/documentSlice';
-import modalReducer from './features/modal/modalSlice';
 
 export const store = configureStore({
   reducer: {
     app: appReducer,
     user: userReducer,
     note: noteReducer,
-    document: documentReducer,
-    modal: modalReducer
   },
 });
