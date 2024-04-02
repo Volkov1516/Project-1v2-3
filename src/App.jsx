3ead4c26b235970c0a49d57c9b19c307bcd9bba7
Vlad Volkov <from5jforever@gmail.com>
2024-04-02 18:42:55 +0300

    move user observer to thunk

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { setTheme, setSnackbar } from 'redux/features/app/appSlice';
-import { setUser } from 'redux/features/user/userSlice';
-import { auth, db } from 'firebase.js';
-import { onAuthStateChanged } from 'firebase/auth';
-import { doc, getDoc } from 'firebase/firestore';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setTheme } from 'redux/features/app/appSlice';
+import { observeAuthState } from 'redux/features/user/userSlice';
 
 import { Auth } from 'components/pages/Auth/Auth';
 import { Home } from 'components/pages/Home/Home';
@@ -14,46 +11,10 @@ import css from './App.module.css';
 export const App = () => {
   const dispatch = useDispatch();
 
-  const [loading, setLoading] = useState(true);
-  const [logged, setLogged] = useState(false);
+  const { logged, loading } = useSelector(state => state.user);
 
   useEffect(() => {
-    const getUser = async (id, email, name, photo) => {
-      try {
-        const docRef = doc(db, 'users', id);
-        const docSnap = await getDoc(docRef);
-
-
-        dispatch(setUser({
-          id,
-          email,
-          name: docSnap?.data()?.name || name || null,
-          photo: docSnap?.data()?.photo || photo || null,
-          documents: docSnap?.data()?.documents || {
-            id: 'root',
-            folders: [],
-            notes: [],
-            tasks: []
-          }
-        }));
-
-        setLogged(true);
-        setLoading(false);
-      } catch (error) {
-        dispatch(setSnackbar('Error receiving data'));
-      }
-    };
-
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        getUser(user?.uid, user?.email, user?.displayName, user?.photoURL);
-      } else {
-        setLogged(false);
-        setLoading(false);
-      }
-    });
-
-    return () => unsubscribe();
+    dispatch(observeAuthState());
   }, [dispatch]);
 
   useEffect(() => {
@@ -91,9 +52,5 @@ export const App = () => {
     );
   }
 
-  return (
-    <div>
-      {logged ? <Home /> : <Auth />}
-    </div>
-  );
+  return logged ? <Home /> : <Auth />;
 };
--- src/redux/features/user/userSlice.js
+++ src/redux/features/user/userSlice.js
@@ -1,4 +1,9 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { setSnackbar } from '../app/appSlice.js';
+
+import { db, auth } from '../../../firebase.js';
+import { doc, getDoc } from 'firebase/firestore';
+import { onAuthStateChanged } from 'firebase/auth';
 
 const initialState = {
   userId: null,
@@ -7,7 +12,10 @@ const initialState = {
   userPhoto: null,
   documents: null,
   path: ['root'],
-  activeTaskId: null
+  activeTaskId: null,
+  logged: false,
+  loading: true,
+  error: null
 };
 
 export const userSlice = createSlice({
@@ -36,8 +44,62 @@ export const userSlice = createSlice({
     setActiveTaskId: (state, action) => {
       state.activeTaskId = action.payload;
     },
+    setLogged: (state, action) => {
+      state.logged = action.payload;
+    },
+    setLoading: (state, action) => {
+      state.loading = action.payload;
+    },
+    setError: (state, action) => {
+      state.error = action.payload;
+    },
   }
 });
 
-export const { setUser, updateUserName, updateUserPhoto, updateDocuments, updatePath, setActiveTaskId } = userSlice.actions;
+export const observeAuthState = () => dispatch => {
+  const unsubscribe = onAuthStateChanged(auth, async user => {
+    if (user) {
+      try {
+        const docRef = doc(db, 'users', user?.uid);
+        const docSnap = await getDoc(docRef);
+
+        dispatch(setUser({
+          id: user?.uid,
+          email: user?.email,
+          name: docSnap?.data()?.name || user?.displayName || null,
+          photo: docSnap?.data()?.photo || user?.photoURL || null,
+          documents: docSnap?.data()?.documents || {
+            id: 'root',
+            folders: [],
+            notes: [],
+            tasks: []
+          }
+        }));
+        dispatch(setLogged(true));
+      } catch (error) {
+        dispatch(setLogged(false));
+        dispatch(setSnackbar('Error receiving data'));
+        dispatch(setError(error));
+      }
+    } else {
+      dispatch(setLogged(false));
+    }
+
+    dispatch(setLoading(false));
+  });
+
+  return () => unsubscribe();
+};
+
+export const {
+  setUser,
+  updateUserName,
+  updateUserPhoto,
+  updateDocuments,
+  updatePath,
+  setActiveTaskId,
+  setLogged,
+  setLoading,
+  setError
+} = userSlice.actions;
 export default userSlice.reducer;
