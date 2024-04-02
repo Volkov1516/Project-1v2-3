88b86bc825e3124e6d9a7602519534ac4a48af8d
Vlad Volkov <from5jforever@gmail.com>
2024-04-02 19:28:36 +0300

    refactor

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,45 +1,12 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { setTheme } from 'redux/features/app/appSlice';
-
 import { Bar } from './Bar/Bar';
 import { Manager } from './Manager/Manager';
 import { EditorModal } from './EditorModal/EditorModal';
 import { Snackbar } from 'components/atoms/Snackbar/Snackbar';
-
-import css from './Home.module.css';
 import { Route } from 'components/atoms/Navigation/Route';
 
+import css from './Home.module.css';
+
 export const Home = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const body = document.body;
-    const theme = localStorage.getItem('theme');
-
-    if (theme) {
-      if (theme === 'light') {
-        body.classList.remove('dark-theme');
-        body.classList.add('light-theme');
-
-        dispatch(setTheme('light'));
-      }
-      else if (theme === 'dark') {
-        body.classList.remove('light-theme');
-        body.classList.add('dark-theme');
-
-        dispatch(setTheme('dark'));
-      }
-    }
-    else {
-      if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        dispatch(setTheme('dark'));
-      } else {
-        dispatch(setTheme('light'));
-      }
-    }
-  }, [dispatch]);
-
   return (
     <div className={css.container}>
       <Bar />
