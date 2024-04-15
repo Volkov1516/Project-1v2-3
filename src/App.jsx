7e912ab034acf9e0291bb9a3649051dcff327a45
Vlad Volkov <from5jforever@gmail.com>
2024-04-15 09:52:49 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -79,7 +79,7 @@ export const App = () => {
 
     document.addEventListener('touchstart', (e) => startX = e.touches[0].clientX);
 
-    document.addEventListener('touchend', (e) => exnX = e.changedTouches[0].clientX);
+    document.addEventListener('touchend', (e) => endX = e.changedTouches[0].clientX);
 
     if (startX < edgeThreshold && startX - endX > threshold) alert('toch');
   }, []);
