20b1f810ce9e7961ad2b6f47cab977095bc9b9ea
Vlad Volkov <from5jforever@gmail.com>
2024-05-18 15:28:13 +0300

    test

--- src/components/atoms/DragAndDrop/DragAndDropElement.jsx
+++ src/components/atoms/DragAndDrop/DragAndDropElement.jsx
@@ -1,24 +1,40 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
 import { useDragAndDrop } from './DragAndDropContext';
 
 export const DragAdnDropElement = ({ children, index, id, type, name, openSettingsModal }) => {
+  const { windowWidth } = useSelector(state => state.app);
+
   const { dragStart, dragMove, dragEnd } = useDragAndDrop();
 
-  return (
+  return windowWidth < 639 ? (
     <div
       id={id}
       data-index={index}
       data-id={id}
       data-type={type}
-      // onMouseDown={(e) => dragStart(e, index, id, type, name, openSettingsModal)}
-      // onMouseMove={dragMove}
-      // onMouseUp={dragEnd}
       onTouchStart={(e) => dragStart(e, index, id, type, name, openSettingsModal)}
       onTouchMove={dragMove}
       onTouchEnd={dragEnd}
-      // style={{ transition: 'transform 0.2s ease-in' }}
+      style={{ transition: 'transform 0.2s ease-in' }}
     >
       {children}
     </div>
-  );
+  ) : (
+    <div
+      id={id}
+      data-index={index}
+      data-id={id}
+      data-type={type}
+      onTouchStart={(e) => dragStart(e, index, id, type, name, openSettingsModal)}
+      onTouchMove={dragMove}
+      onTouchEnd={dragEnd}
+      onMouseDown={(e) => dragStart(e, index, id, type, name, openSettingsModal)}
+      onMouseMove={dragMove}
+      onMouseUp={dragEnd}
+    >
+      {children}
+    </div>
+  )
 };
