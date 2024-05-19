eace1a4ffc891d0468bde19b26acd2b448a01087
Vlad Volkov <from5jforever@gmail.com>
2024-05-19 16:47:10 +0300

    widgets component

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,15 +1,21 @@
+import { useSelector } from 'react-redux';
+
 import { Bar } from './Bar/Bar';
 import { Manager } from './Manager/Manager';
+import { Widgets } from './Widgets/Widgets';
 import { EditorModal } from './EditorModal/EditorModal';
 import { Snackbar } from 'components/Snackbar/Snackbar';
 
 import css from './Home.module.css';
 
 export const Home = () => {
+  const { windowWidth } = useSelector(state => state.app);
+
   return (
     <div className={css.container}>
       <Bar />
       <Manager />
+      {windowWidth > 639 && <Widgets />}
       <EditorModal />
       <Snackbar />
     </div>
