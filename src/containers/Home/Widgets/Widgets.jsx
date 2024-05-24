2840879c7e5bbbfd0047707414ea633e303c5be6
Vlad Volkov <from5jforever@gmail.com>
2024-05-24 12:58:52 +0300

    remove clock widget

--- src/containers/Home/Widgets/Widgets.jsx
+++ src/containers/Home/Widgets/Widgets.jsx
@@ -1,21 +1,9 @@
-import {useState, useEffect} from 'react';
-
 import css from './Widgets.module.css';
 
 export const Widgets = () => {
-  const [time, setTime] = useState(new Date());
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
-
-    return () => clearInterval(intervalId);
-  }, []);
-
   return (
     <div className={css.container}>
-      {time.getHours() + ":" + (time.getMinutes() < 10 ? ("0" + time.getMinutes()) : time.getMinutes()) + ":" + (time.getSeconds() < 10 ? ("0" + time.getSeconds()) : time.getSeconds())}
+      
     </div>
   );
 };
