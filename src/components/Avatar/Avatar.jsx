8e40791e162e45c2f3e38336d4f9d5e2919447b7
Vlad Volkov <from5jforever@gmail.com>
2024-05-28 19:14:39 +0300

    fix

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,33 +1,37 @@
 import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 
 import css from './Avatar.module.css';
 
-import avatar from 'assets/images/avatar.svg';
+import avatarLight from 'assets/images/avatarLight.svg';
+import avatarDark from 'assets/images/avatarDark.svg';
 
 export const Avatar = ({ src, alt, size, onClick }) => {
+  const { theme } = useSelector(state => state.app);
+
   const [imgSrc, setImgSrc] = useState(src);
 
   useEffect(() => {
     setImgSrc(src);
-  }, [src]);
+  }, [src, theme]);
 
-  const handleError = () => setImgSrc(avatar);
+  const handleError = () => theme === 'light' ? setImgSrc(avatarLight) : setImgSrc(avatarDark);
 
   const handleTouchStart = e => e.currentTarget.classList.add(css.touch);
 
   const handleTouchEnd = e => e.currentTarget.classList.remove(css.touch);
 
   return (
-    <img
-      className={`${css.img} ${css[size]}`}
-      src={imgSrc}
-      alt={alt}
-      draggable={false}
-      onClick={onClick}
-      onError={handleError}
-      onContextMenu={(e) => e.preventDefault()}
-      onTouchStart={handleTouchStart}
-      onTouchEnd={handleTouchEnd}
-    />
+      <img
+        className={`${css.img} ${css[size]}`}
+        src={imgSrc}
+        alt={alt}
+        draggable={false}
+        onClick={onClick}
+        onError={handleError}
+        onContextMenu={(e) => e.preventDefault()}
+        onTouchStart={handleTouchStart}
+        onTouchEnd={handleTouchEnd}
+      />
   );
 };
