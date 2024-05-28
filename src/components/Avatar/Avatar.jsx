e585a874014b876f027e23d80b516b7d2f17f2ef
Vlad Volkov <from5jforever@gmail.com>
2024-05-28 17:33:29 +0300

    avatar

--- src/components/Avatar/Avatar.jsx
+++ src/components/Avatar/Avatar.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 
 import css from './Avatar.module.css';
 
+import avatar from 'assets/images/avatar.svg';
+
 export const Avatar = ({ src, alt, size, onClick }) => {
+  const [imgSrc, setImgSrc] = useState(src);
+
+  useEffect(() => {
+    setImgSrc(src);
+  }, [src]);
+
+  const handleError = () => setImgSrc(avatar);
+
   return (
-    <img className={`${css.img} ${css[size]}`} src={src} alt={alt} onClick={onClick} />
+    <img
+      className={`${css.img} ${css[size]}`}
+      src={imgSrc}
+      alt={alt}
+      draggable={false}
+      onClick={onClick}
+      onError={handleError}
+    />
   );
 };
